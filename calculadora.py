ecafe39f46bdabad0ab817319f2148d47fac5490
neilkevinmc-wq <neil.kevin.mc@gmail.com>
2026-07-22 15:45:39 -0500

    Update calculadora.py
    AGREGAR LA RESTA
    
    Neil Meza

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,21 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
-# ===========================================
+## CALCULADORA - VERSIÓN 2
+# Autor(a): NEIL MEZA
+
+print("CALCULADORA")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n1. Sumar")
+print("2. Restar")
 
-print("Resultado:", suma)
+opcion = input("\nSeleccione una opción: ")
+
+if opcion == "1":
+    print("\nLa suma es:", num1 + num2)
+
+elif opcion == "2":
+    print("\nLa resta es:", num1 - num2)
+
+else:
+    print("\nOpción no válida.")
