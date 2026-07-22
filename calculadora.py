651541044314a248a17e815f23d8a5d2afb3070f
neilkevinmc-wq <neil.kevin.mc@gmail.com>
2026-07-22 15:53:52 -0500

    Update calculadora.py
    AGREGAR DIVISIÓN
    
    By Neil Meza

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 3
+## CALCULADORA - VERSIÓN 4
 # Autor(a): NEIL MEZA
 
 print("CALCULADORA")
@@ -9,6 +9,7 @@ num2 = float(input("Ingrese el segundo número: "))
 print("\n1. Sumar")
 print("2. Restar")
 print("3. Multiplicar")
+print("4. Dividir")
 
 opcion = input("\nSeleccione una opción: ")
 
@@ -20,6 +21,12 @@ elif opcion == "2":
 
 elif opcion == "3":
     print("\nLa multiplicación es:", num1 * num2)
+
+elif opcion == "4":
+    if num2 != 0:
+        print("\nLa División es:", num1 / num2)
+    else:
+        print("No se puede dividir entre cero.")
 
 else:
     print("\nOpción no válida.")
